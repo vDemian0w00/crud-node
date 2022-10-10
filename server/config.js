66477ae952afb714646f1afc5cfaea7d9dca5b24
vDemian0w00 <cfajardoa2000@alumno.ipn.mx>
2022-10-10 10:50:48 -0500

    commit final3

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
-export const PORT = process.env.PORT || 3000
+export const PORT = process.env.PORT || 2020
 
 export const DB_HOST = process.env.DB_HOST || 'localhost'
 
--- server/index.js
+++ server/index.js
@@ -9,12 +9,11 @@ import mainRoutes from "./routes/ghibli.routes.js";
 
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url))
-app.use(cors({
-    origin: 'http://localhost:5173'
-}))
+app.use(cors())
 app.use(express.json())
 
 app.use(indexRoutes, mainRoutes);
 app.use(express.static(join(__dirname, '../client/dist')))
+console.log(join(__dirname, '../client/dist'));
 app.listen(PORT);
 console.log(`Server ejecutandose en puerto: ${PORT}`);
