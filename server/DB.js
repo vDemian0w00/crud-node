05244cce0583b9ec48174437525fedc556a82b10
vDemian0w00 <cfajardoa2000@alumno.ipn.mx>
2022-10-12 01:54:58 -0500

    prueba ssl sin timeout

--- server/DB.js
+++ server/DB.js
@@ -1,8 +1,6 @@
 import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
-  connectionLimit: 10,
-  acquireTimeout: 30000, //30 secs
   host: 'containers-us-west-98.railway.app',
   port: 5934,
   user: 'root',
