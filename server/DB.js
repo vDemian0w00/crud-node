4ed978ee7771b3572a708fac7d964b6656fe0bd7
vDemian0w00 <cfajardoa2000@alumno.ipn.mx>
2022-10-12 01:58:43 -0500

    prueba PS

--- server/DB.js
+++ server/DB.js
@@ -1,11 +1,10 @@
 import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
-  host: 'containers-us-west-98.railway.app',
-  port: 5934,
-  user: 'root',
-  password: 'ZlVUcgLAGLdlIXEckjWM',
-  database: "railway",
+  host: 'us-east.connect.psdb.cloud',
+  user: 'al9f9p9gnxogx9v1zu3k',
+  password: 'pscale_pw_AVx1s4gbDvgxcZA70xUBmRP12BIEBUxS4iTA4TvUoVs',
+  database: "ghibli",
   ssl: {
     rejectUnauthorized: false
   }
